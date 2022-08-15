5e91ecb51c986d0c485631cb864ba4c7efb8c16c
sialvsic <sialvsic@outlook.com>
2022-08-15 15:36:21 +0800

    update l-2

--- leetcode/2.js
+++ leetcode/2.js
@@ -35,76 +35,41 @@ function appendList(nums) {
   return next;
 }
 
-function getLength(l) {
-  if (!l) {
-    return 0;
-  }
-
-  let sum = 1;
-
-  while (l && l.next !== null) {
-    l = l.next;
-    sum = sum + 1;
-  }
-
-  return sum;
-}
-
 /**
  * @param {ListNode} l1
  * @param {ListNode} l2
  * @return {ListNode}
  */
 var addTwoNumbers = function (l1, l2) {
-  let flag = 0;
-  const length1 = getLength(l1);
-  const length2 = getLength(l2);
-  const max = Math.max(length1, length2);
+  let addOne = 0;
+  let flag = (cur = new ListNode(0));
 
-  let arr = [];
-  let l = null;
+  while (addOne || l1 || l2) {
+    let val1 = l1 !== null ? l1.val : 0;
+    let val2 = l2 !== null ? l2.val : 0;
+    let sum = val1 + val2 + addOne;
 
-  let p1 = l1;
-  let p2 = l2;
+    addOne = sum >= 10 ? 1 : 0;
 
-  for (let index = 0; index < max; index++) {
-    let v1 = (p1 && p1.val) || 0;
-    let v2 = (p2 && p2.val) || 0;
-    let result = v1 + v2 + flag;
+    cur.next = new ListNode(sum % 10);
+    cur = cur.next;
 
-    //进位计算
-    if (result >= 10) {
-      result = result - 10;
-      flag = 1;
-    } else {
-      flag = 0;
+    if (l1) {
+      l1 = l1.next;
     }
-
-    if (p1) {
-      p1 = p1.next;
+    if (l2) {
+      l2 = l2.next;
     }
-
-    if (p2) {
-      p2 = p2.next;
-    }
-
-    arr.push(result);
   }
 
-  if (flag == 1) {
-    arr.push(1);
-  }
-
-  l = appendList(arr);
-
-  return l;
+  return flag.next;
 };
 
-// const l1 = [2, 4, 3],
-//   l2 = [5, 6, 4, 2];
+const l1 = [2, 4, 3],
+  l2 = [5, 6, 4, 2];
 
-const l1 = [9, 9, 9, 9, 9, 9, 9],
-  l2 = [9, 9, 9, 9];
+// const l1 = [9, 9, 9, 9, 9, 9, 9],
+//   l2 = [9, 9, 9, 9];
 
 const list1 = appendList(l1);
 const list2 = appendList(l2);
@@ -112,5 +77,5 @@ const list2 = appendList(l2);
 //[7,0,8] 342 + 465 = 807
 //[7,0,8] 342 + 2465 = 2807
 
-const r = addTwoNumbers(list1, list2); //[7,0,8]
+const r = addTwoNumbers(list1, list2); //[7,0,8,2]
 console.log("r", JSON.stringify(r));
